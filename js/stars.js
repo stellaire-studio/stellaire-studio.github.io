b1657238c551787803c4f576ab12cc7946e1af74
Rakido <jeanjulien.bernard@gmail.com>
2022-07-08 22:18:12 +0200

    New star animation timing

--- js/stars.js
+++ js/stars.js
@@ -1,25 +1,23 @@
 let tl2 = anime.timeline({
+}).add({
+  targets: '.star',
+  scale: [
+    {value: 0.8, easing: 'easeOutSine', duration: 300},
+    {value: 1, easing: 'easeInOutQuad', duration: 2000},
+  ],
+  delay: anime.stagger(70, {grid: [50, 5], from: 'center'})
+})
 
-    }).add({
-      targets: '.star',
-      scale: [
-          {value: 0, easing: 'easeOutSine', duration: 300},
-          {value: 1, easing: 'easeInOutQuad', duration: 2000},
-        ],
-        delay: anime.stagger(200, {grid: [50, 5], from: 'center'})
-    })
+let tl = anime.timeline({
+  loop:true
+})
+.add({
+  targets: '.star',
+  opacity: [
+    {value: 0, easing: 'easeOutSine', duration: 100},
+    {value: 0.55, easing: 'easeInOutQuad', duration: 6000},
+    {value: 0, easing: 'easeInOutQuad', duration: 2000}
+  ],
+  delay: anime.stagger(60, {grid: [50, 5], from: 'center'})
+});
 
-    let tl = anime.timeline({
-      loop:true
-    })
-    .add({
-        targets: '.star',
-
-        opacity: [
-          {value: 0, easing: 'easeOutSine', duration: 300},
-          {value: 0.3, easing: 'easeInOutQuad', duration: 6000},
-          {value: 0, easing: 'easeInOutQuad', duration: 2000}
-        ],
-
-        delay: anime.stagger(200, {grid: [50, 5], from: 'center'})
-      });
