537c84e7300029516ad4a4b2a3b61e2e715df00c
Nicolas Filzi <nfilzi.webservices@gmail.com>
2023-03-30 17:48:47 +0200

    Handle visitor preferred default theme + local override based on data attribute
    1. Local HTML override through data attribute
    2. Visitor preferred default theme
    3. Default to light theme

--- assets/js/faq-tooltip.js
+++ assets/js/faq-tooltip.js
@@ -1,13 +1,18 @@
+const getPreferredScheme = () => window?.matchMedia?.('(prefers-color-scheme:dark)')?.matches ? 'stellaire-dark' : 'stellaire-light';
+
 let faqsTooltips = document.querySelectorAll('.faq-tooltip')
 
 faqsTooltips.forEach((faqTooltip) => {
-  content = faqTooltip.querySelector('.faq-tooltip-content').innerHTML
+  let content = faqTooltip.querySelector('.faq-tooltip-content').innerHTML
+
+  let visitorThemeMode = getPreferredScheme()
+  let tooltipTheme     = faqTooltip.dataset.tooltipTheme || getPreferredScheme() || 'stellaire-light'
 
   tippy(faqTooltip, {
     content: content,
-    theme: 'stellaire-dark',
+    theme: tooltipTheme,
     maxWidth: 500,
     allowHTML: true,
-    arrow: false,
+    arrow: false
   });
 });
